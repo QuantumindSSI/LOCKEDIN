fad2e5299e74fb5ec1bcf354b62cfbcab8fd54ea
Quantumind SSI <engineering@quantumind.io>
2026-06-01 18:20:50 +0100

    Fix TrainingArguments - evaluation_strategy renamed to eval_strategy in newer transformers

--- task_01_quantum_crypto_analyzer/scripts/train.py
+++ task_01_quantum_crypto_analyzer/scripts/train.py
@@ -207,7 +207,7 @@ def main():
         logging_steps=config.logging_steps,
         save_steps=config.save_steps,
         eval_steps=config.eval_steps,
-        evaluation_strategy="steps",
+        eval_strategy="steps",
         save_total_limit=3,
         fp16=config.fp16,
         load_best_model_at_end=True,
